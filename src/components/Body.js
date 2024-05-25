7f712aa5782101c2bbbe99169215f44c69389318
Ketan188 <ketanpatil188@gmail.com>
2024-05-26 02:06:32 +0530

    Basic search bar implementation

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,50 @@
-import RestaurantCard from './RestaurantCard'
-import { restaurantsList } from './config'
+import RestaurantCard from "./RestaurantCard";
+import { restaurantsList } from "./config";
+import { useState } from "react";
 
 const Body = () => {
-    return (
-        <>
-            <div className="body-container">
-                {
-                    restaurantsList.map(restaurant => {
-                        //using spred props
-                        return <RestaurantCard {...restaurant.info} key={restaurant.info.id}/>
+  //useState to update variable state
 
-                        // return <RestaurantCard restaurnatDetails={restaurant} />
-                    })
-                }
-            </div>  
-        </>
-    )
-}
+  const [searchText, setSearchText] = useState("");
+  const [restaurants, setRestaurants] = useState(restaurantsList);
+
+  function filterResults(testValue, restaurants) {
+    const filteredList = restaurants.filter((data) =>
+      data.info.name.toLowerCase().includes(testValue.toLowerCase())
+    );
+    setRestaurants(filteredList);
+  }
+
+  return (
+    <>
+      <div className="body-container">
+        <div className="search-bar-wrap">
+          <input
+            type="text"
+            placeholder="Search"
+            name="searchbar"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button onClick={() => filterResults(searchText, restaurantsList)}>
+            Search
+          </button>
+        </div>
+        <div className="card-wrap">
+          {restaurants.map((restaurant) => {
+            //using spred props
+            return (
+              <RestaurantCard {...restaurant.info} key={restaurant.info.id} />
+            );
+
+            // return <RestaurantCard restaurnatDetails={restaurant} />
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Body;
